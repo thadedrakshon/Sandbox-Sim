5cdc633ba8bfc6d31b5d8a8221b3d7aa5c617e07
thadedrakshon <nealalbrechtjohnson@gmail.com>
2025-05-24 22:16:10 -0500

    Setup github web launch

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,13 +1,17 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Sandbox-Sim/',
   root: './',
   publicDir: 'src/assets',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
+    emptyOutDir: true,
+    sourcemap: true
   },
   server: {
     port: 3000,
     open: true
   }
-}
+});
